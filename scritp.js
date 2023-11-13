2741ca4b2b3ff09f0f70dbd98dc65abb4f7731ab
DevJunior <criticossemdiploma@gmail.com>
2023-11-13 08:42:14 -0300

    primeiro commit

---
+++ scritp.js
@@ -0,0 +1,34 @@
+
+
+function exibirCor() {
+    var r, g, b, t;
+
+    r = Number(document.getElementById("slideRed").value);
+    g = Number(document.getElementById("slideGreen").value);
+    b = Number(document.getElementById("slideBlue").value);
+    t = Number(document.getElementById("slideTranspa").value);
+
+    var transpa = `0.${t}`
+
+
+
+    document.getElementById("valorRed").innerHTML = r;
+    document.getElementById("valorGreen").innerHTML = g;
+    document.getElementById("valorBlue").innerHTML = b;
+    document.getElementById("valorTranspa").innerHTML = `0.${t}`;
+
+    document.getElementById("rgbtexto").innerHTML = `rgb(${r},${g},${b},0.${t})`
+
+
+    document.getElementById("rgbcor").style.backgroundColor = `rgb(${r}, ${g}, ${b},0.${t})`
+
+    document.getElementById('hexatexto').innerHTML = '#' + [r,g,b,t].map( x=> parseInt(x).toString(16).padStart(2, 0)).join('')
+
+
+}
+
+
+
+
+
+
